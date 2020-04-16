a24b3355b6765540370f79212b7a0970fac6659d
TimeIsTheChoice <yuhang.tao.email@gmail.com>
2020-04-16 14:47:16 +0800

    dice_loss

--- examples/pycaffe/layers/dice_loss.py
+++ examples/pycaffe/layers/dice_loss.py
@@ -2,7 +2,7 @@ import caffe
 import numpy as np
 import scipy
 
-class Multi_Loss(caffe.Layer):
+class Dice_Loss(caffe.Layer):
     """
     Compute the Euclidean Loss in the same manner as the C++ EuclideanLossLayer
     to demonstrate the class interface for developing layers in Python.
@@ -23,25 +23,22 @@ class Multi_Loss(caffe.Layer):
         top[0].reshape(1)
 
     def forward(self, bottom, top):
-        self.pd = bottom[0].data
+        self.pd= bottom[0].data
         self.gt = bottom[1].data
-        self.diff[...]=bottom[1].data
-        smooth = 1.    
-        self.sum=bottom[0].data.sum()+bottom[1].data.sum()+smooth
-        self.dice=(2.* (bottom[0].data * bottom [1].data).sum()+smooth)/self.sum
+        
+        # self.diff[...]=bottom[1].data
+        smooth = 1.
+        self.intersection =  np.sum(self.pd* self.gt)   
+        self.sum=np.sum(bottom[0].data)+np.sum(bottom[1].data.sum())+smooth
+        self.dice=(2.* self.intersection+smooth)/self.sum
         top[0].data[...] = 1.- self.dice
 
-    # def backward(self, top, propagate_down, bottom):
-    #     if propagate_down[1]:
-    #         raise Exception("label not diff")
-    #     elif propagate_down[0]:
-    #         bottom[0].diff[...] = -2.*(self.diff**2)/(self.sum**2)
-    #     else:
-    #         raise Exception("no diff")
     def backward(self, top, propagate_down, bottom):
-          if propagate_down[1]:
+        if propagate_down[1]:
             raise Exception("label not diff")
-          elif propagate_down[0]:
-            numerator = self.gt*(np.power(self.pd, 2)+np.power(self.gt,*2).sum()-2*self.pd*(self.gt*self.pd).sum()
-            denominator = np.power((np.power(self.pd, 2)+np.power(self.gt,*2)),2)
-            bottom[0].diff[...] = -2.*numerator/denominator
+        elif propagate_down[0]:
+            numerator =  2.*np.power(self.gt,2)
+            denominator = np.power((np.power(self.pd, 1)+np.power(self.gt,1)),2)
+            derivative = numerator/(denominator)/bottom[0].num
+            # print(derivative.shape, bottom[0].num)
+            bottom[0].diff[...] = derivative
--- examples/pycaffe/layers/multi_loss.py
+++ examples/pycaffe/layers/multi_loss.py
@@ -1,6 +1,6 @@
 import caffe
 import numpy as np
-
+import scipy
 
 class Multi_Loss(caffe.Layer):
     """
@@ -12,26 +12,33 @@ class Multi_Loss(caffe.Layer):
         # check input pair
         if len(bottom) != 2:
             raise Exception("Need two inputs to compute distance.")
+        self.batch_size=bottom[1].data.shape[0]
 
     def reshape(self, bottom, top):
         # check input dimensions match
-        if bottom[0].count != bottom[1].count:
+        if bottom[0].count!=bottom[1].count:
             raise Exception("Inputs must have the same dimension.")
-        # difference is shape of inputs
-        self.diff = np.zeros_like(bottom[0].data, dtype=np.float32)
+        self.diff=np.zeros_like(bottom[0].data,dtype=np.float32)
         # loss output is scalar
         top[0].reshape(1)
 
     def forward(self, bottom, top):
-        self.diff[...] = bottom[0].data - bottom[1].data
-        top[0].data[...] = np.sum(self.diff**2) / bottom[0].num / 2.
+        self.pd= bottom[0].data
+        self.gt = bottom[1].data
+        
+        # self.diff[...]=bottom[1].data
+        smooth = 1.
+        self.intersection =  np.sum(self.pd* self.gt)   
+        self.sum=np.sum(bottom[0].data)+np.sum(bottom[1].data.sum())+smooth
+        self.dice=(2.* self.intersection+smooth)/self.sum
+        top[0].data[...] = 1.- self.dice
 
     def backward(self, top, propagate_down, bottom):
-        for i in range(2):
-            if not propagate_down[i]:
-                continue
-            if i == 0:
-                sign = 1
-            else:
-                sign = -1
-            bottom[i].diff[...] = sign * self.diff / bottom[i].num
+        if propagate_down[1]:
+            raise Exception("label not diff")
+        elif propagate_down[0]:
+            numerator = self.gt*np.sum(np.power(self.pd, 2)+np.power(self.gt,2))-2*self.pd*np.sum(self.gt*self.pd)
+            denominator = np.power((np.power(self.pd, 2)+np.power(self.gt,2)),2)
+            derivative = -2.*(numerator+9e-8)/(denominator+9e-8)/bottom[0].num
+            # print(derivative.shape, bottom[0].num)
+            bottom[0].diff[...] = derivative
